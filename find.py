f6c2033acbd473e8016cea77595cda4440f7e51a
monumk <73218094+monumk@users.noreply.github.com>
2021-06-26 23:11:54 +0530

    Add files via upload

---
+++ find.py
@@ -0,0 +1,14 @@
+'''Find the index of the first occurrence of a substring in a string'''
+
+'''find function return index value of first occurance in string not found it returns -1'''
+
+
+str1='Govt sr sec school faridabad'
+print(str1.find('school'))
+print(str1.find('nit'))
+
+
+'''index function return index value of first occurance in string not found value error '''
+
+print(str1.index('faridabad')) 
+print(str1.index('nit')) 
